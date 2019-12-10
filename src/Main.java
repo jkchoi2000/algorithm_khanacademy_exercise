092709119d3c607d8d29d156a4752160fb8633ac
jkchoi2000 <jkchoi2000@gmail.com>
2019-12-10 16:01:46 +0900

    병합정렬(MergeSort) 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import mine.jkchoi.algorithm.BinarySearcher;
 import mine.jkchoi.algorithm.InsertionSort;
+import mine.jkchoi.algorithm.MergeSort;
 
 import java.util.Arrays;
 
@@ -7,10 +8,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final int[] primes = new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        final int[] primes = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
         System.out.println(BinarySearcher.search(primes, 67));
 
-        final int[] insertionSortArray = new int[] {5, 6, 1, 2, 4, 3};
+        //삽입 정렬
+        final int[] insertionSortArray = new int[]{5, 6, 1, 2, 4, 3};
         System.out.println(Arrays.toString(insertionSortArray) + " => " + Arrays.toString(InsertionSort.sort(insertionSortArray)));
+
+        //합병 (머지, 병합) 정렬
+        final int[] mergeSortArray = new int[]{5, 2, 4, 7, 6, 1, 3, 8};
+        System.out.println(Arrays.toString(mergeSortArray) + " => " + Arrays.toString(MergeSort.sort(mergeSortArray)));
+
     }
 }
+
---
+++ src/mine/jkchoi/algorithm/MergeSort.java
@@ -0,0 +1,51 @@
+package mine.jkchoi.algorithm;
+
+public class MergeSort {
+    public static int[] sort(int[] array) {
+        if (array == null || array.length == 0) {
+            return array;
+        }
+
+        sort(array, 0,  array.length);
+
+        return array;
+    }
+
+    private static void sort(int[] array, int low, int high) {
+        if(high - low < 2) {
+            return;
+        }
+
+        int mid = (low + high) / 2;
+
+        sort(array, low, mid);
+        sort(array, mid, high);
+
+        merge(array, low, mid, high);
+    }
+
+    private static void merge(int[] array, int low, int mid, int high) {
+        int[] tmp = new int[high - low];
+        int t = 0, l = low, h = mid;
+
+        while (l < mid && h < high) {
+            if(array[l] < array[h]) {
+                tmp[t++] = array[l++];
+            } else {
+                tmp[t++] = array[h++];
+            }
+        }
+
+        while(l < mid) {
+            tmp[t++] = array[l++];
+        }
+
+        while(h < high) {
+            tmp[t++] = array[h++];
+        }
+
+        for(int i = low; i < high; i++) {
+            array[i] = tmp[i - low];
+        }
+    }
+}
